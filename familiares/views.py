b5e34450418a177c9feca2985a575e4fc39f8def
Tobias Carballo <tobiascarballo02@gmail.com>
2022-08-28 05:10:22 -0300

    solcionado, falta agregar url de nacionalidad

--- familiares/views.py
+++ familiares/views.py
@@ -6,7 +6,7 @@ from familiares.models import familiares
 def listar_familiares(request):
     queryset = familiares.objects.all()
     diccionario = {'familiares': queryset}
-    plantilla = loader.get_template('listar_familiares.html')
+    plantilla = loader.get_template('lista_familiares.html')
     documento_html = plantilla.render(diccionario)
 
     return HttpResponse(documento_html)
